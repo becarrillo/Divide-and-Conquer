b33550db5e68993a993d6c561f7357b56ad1919d
brando <belicarrillo30@gmail.com>
2022-06-07 11:28:24 -0500

    Resolución de ejercicio de Recursividad realizado exitosamente

--- divide_and_conquer.py
+++ divide_and_conquer.py
@@ -1,26 +1,31 @@
-vector_numeros = [5, 2, 4, 7, 1, 3, 2, 6]
-n = len(vector_numeros)
+def merge_sort(A,p,r):
+    if p < r:
+        q = (p+r) // 2
+        merge_sort(A,p,q)
+        merge_sort(A,q+1,r)
+        merge(A,p,q,r)
 
-def MERGE_SORT(A, p, r):
-    if (p < r):   
-        
-        q = round((p + r) / 2)
-        left = A[p-1:q]
-        right = A[q-1:r]
-        left.sort()
-        right.sort()
-        ul = left + right
-        ul.sort()
-        
-        MERGE_SORT(A, p, q)
-        MERGE_SORT(A, q+1, r)
-        MERGE(A, p, q, r)
-    else:
-        A.sort()
-       
-def MERGE(A, p, q, r):
-    if (A[p] < A[q] and A[q] < A[r]):
-        return A[p-1:q] + A[q:r]
-        
-merge_sort_call = MERGE_SORT(vector_numeros, 1, n)
-print(merge_sort_call)
+def merge(A,p,q,r):
+    left = A[p:q+1]
+    right = A[q+1:r+1]
+
+    left.append(float("inf"))
+    right.append(float("inf"))
+    
+    i = 0
+    j = 0
+    k = p
+
+    while k <= r and j < len(right) and i < len(left):
+        if left[i] > right[j]:
+            A[k] = right[j]
+            j += 1
+        else:
+            A[k] = left[i]
+            i += 1
+        k += 1
+
+lista_A = [5,2,4,7,1,3,2,6]
+
+merge_sort(lista_A,0,7)
+print(lista_A)
